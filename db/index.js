2ea21461b0e6df491825c704f8efc3f7a2f5a8da
lijian <lijian@baijiayun.com>
2021-04-11 23:25:49 +0800

    refactor: 加入mongodb权限校验

--- db/index.js
+++ db/index.js
@@ -1,12 +1,24 @@
 let MongoClient = require('mongodb').MongoClient;
-let url = 'mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb';
+// let url = 'mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb';
+let url = 'mongodb://1.15.98.145:27027/?gssapiServiceName=mongodb';
+let test = require('./directory');
 
 exports.connect = function () {
-	MongoClient.connect(url, function (err, db) {
-		if (err) throw err;
-		console.log('数据库连接成功');
-		global.dbSword = db.db('sword');
-		global.article = dbSword.collection('article');
-		global.directory = dbSword.collection('directory');
-	});
+	MongoClient.connect(
+		url,
+		{
+			authSource: 'sword',
+			auth: {
+				user: 'lijian',
+				password: 'yxz09231226',
+			},
+		},
+		function (err, client) {
+			if (err) throw err;
+			console.log('数据库连接成功');
+			global.dbSword = client.db('sword');
+			global.article = dbSword.collection('article');
+			global.directory = dbSword.collection('directory');
+		}
+	);
 };
